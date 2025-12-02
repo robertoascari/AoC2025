2805e129309fc175db0ce31d2dcafcc2cd161f10
Roberto Ascari <roberto.ascari8@gmail.com>
2025-12-02 09:18:01 +0100

    Update Day2.R

--- Day2.R
+++ Day2.R
@@ -1,5 +1,7 @@
 setwd("C:/Users/roberto.ascari/Desktop/R_advent")
+#https://adventofcode.com/2025/day/2#part2
 
+# Part One:
 input <- read.table("Day2.txt", sep = ",")
 test <- read.table("Day2_Example.txt", sep = ",")
 
@@ -35,3 +37,61 @@ for(l in 1:nrow(data_df)){
   }
 }
 count
+
+
+# Part Two:
+rm(list=ls())
+
+setwd("C:/Users/roberto.ascari/Desktop/R_advent")
+input <- read.table("Day2.txt", sep = ",")
+test <- read.table("Day2_Example.txt", sep = ",")
+
+input <- t(input)
+test <- t(test)
+
+
+data <- test
+inizio_ID <- numeric(nrow(data))
+fine_ID <- numeric(nrow(data))
+
+
+for(l in 1:nrow(data)){
+  dash_id <- as.numeric(gregexpr(pattern ="-",data[l]))
+  
+  inizio_ID[l] <- as.numeric(substring(data[l], first = 1, last = dash_id-1))
+  fine_ID[l] <- as.numeric(substring(data[l], first = dash_id+1, last = nchar(data[l])))
+}
+data_df <- data.frame(code=data[,1], inizio_ID=inizio_ID, fine_ID=fine_ID)
+
+
+divisors <- function(x) {
+  y <- seq_len(x)[-1]
+  y[ x%%y == 0 ]
+}
+
+
+
+count_2 <- 0
+for(l in 1:nrow(data_df)){
+  inizio <- data_df$inizio_ID[l]
+  fine <- data_df$fine_ID[l]
+  
+  for(i in inizio:fine){
+    almeno_una_volta <- 0
+    
+    for(div in divisors(nchar(i))){
+      
+      cut <- c(seq(1, nchar(i), by=div), nchar(i)+1)
+      cand <- numeric(length(cut)-1)
+      for(p in 1:(length(cut)-1)){
+        cand[p] <- as.numeric(substring(i, first = cut[p], last = cut[p+1]-1))
+      }
+      if(var(cand) == 0) almeno_una_volta <- almeno_una_volta + 1
+      
+      # gestire diversamente i casi in cui abbiamo un numero primo.
+    }
+    if(almeno_una_volta > 0) count_2 <- count_2 + i
+  }
+}
+count_2
+
